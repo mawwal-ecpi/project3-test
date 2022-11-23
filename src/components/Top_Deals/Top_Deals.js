f186a02561aa778d35557a006b10fc94cfe7c993
Mohammad Awwal <mawwal@rider.edu>
2022-11-22 20:10:36 -0500

    initial code

---
+++ src/components/Top_Deals/Top_Deals.js
@@ -0,0 +1,11 @@
+import React from "react";
+import top_deals from "./Top_Deals.png";
+
+export default function Top_Deals() {
+  return (
+    <div>
+      <h1>TOP DEALS</h1>
+      <img alt="top deals" src={top_deals} />
+    </div>
+  );
+}
